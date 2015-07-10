19ab134e758332d9c39585e0d4857ed6a2ccca5f
Jelena Tabas <jelenatjelena@gmail.com>
2015-07-11 01:05:24 +0200

    random generate boxes

--- desktop/src-common/clo_runner/core.clj
+++ desktop/src-common/clo_runner/core.clj
@@ -25,42 +25,38 @@
                                                 0)
                                             (assoc entity :x (- (:x entity) 30))
                                             (assoc entity :x 0))))
-(defn move-box [entity]
+
+
+(defn create-box []  
+  (assoc (texture "box.jpg") :x 859 :y (rand-int 500) :width 59 :height 59))
+
+(defn move-box [entity] 
    (assoc entity :x (- (:x entity) 3)))
 
 (defn move-boxes [boxes]
   (vec (map move-box boxes)))
 
 (defn moving [entities]    
-      (let [backgr (first entities)
-            player (last entities)
-            boxes (move-boxes (vec (drop 1 (drop-last entities))))]
-      
-     ;;   (->
-     ;;     (conj [] backgr)
-     ;;     (apply conj boxes)
-    ;;      (conj player))
-       (conj (apply conj (conj [] backgr) boxes) player)))
+  (let [backgr (first entities)
+        player (last entities)
+        boxes (try              
+                (move-boxes (vec (drop 1 (drop-last entities))))                
+                (catch NullPointerException e
+                  []))]
+    (conj (apply conj (conj [] backgr) boxes) player)))
 
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
+    (add-timer! screen :spawn-enemy 0 2)
     (let [
-          ; load a sprite sheet from your resources dir
           sheet (texture "bird.png")
           birdy (texture! sheet :split (/ 506 3) (/ 918 5))
           
           background (texture "background.jpg")
-          background (assoc background :x 0 :y 0 :width 800 :height 600)
-          
-          box (texture "box.jpg")
-          box-image (assoc box :x 100 :y 100 :width 59 :height 59)
-          box-image2 (assoc box :x 100 :y 400 :width 59 :height 59)
-          box-image3 (assoc box :x 250 :y 100 :width 59 :height 59)
-          box-image4 (assoc box :x 500 :y 300 :width 59 :height 59)
-          
+          background (assoc background :x 0 :y 0 :width 800 :height 600)                
           
           player-image (texture (aget birdy 0 0))
           ;;images (->
@@ -76,30 +72,24 @@
                 ;;   (conj (texture (aget birdy 1 3)))
                 ;;   (conj (texture (aget birdy 1 4))))
      ;;     player-image (assoc player-image :moving (animation 0.15 images))
-          player-image (assoc player-image :x 10 :y 10 :width 59 :height 59)]
-      ; return a new entities vector with player-image inside of it
-      [background box-image box-image2 box-image3 box-image4 player-image]))
-     ;;[background box-image player-image]))
-  
+          player-image (assoc player-image :x 10 :y 10 :width 59 :height 59)]  
+      [background player-image]))  
   
   :on-render
   (fn [screen entities]
     (clear!)
-   ;; (merge (first entities) (animation->texture screen ((first entities) :moving)))
     (render! screen (moving entities))) 
-  
-    
- ;;dodavanje na on-timer 
-  
-
+       
+  :on-timer
+  (fn [screen entities]
+    :spawn-enemy (conj (conj (vec (drop-last entities)) (create-box)) (last entities)))
   
   :on-key-down
   (fn [screen entities]
     (->
       (drop-last entities)
       (vec)
-      (conj (move screen (last entities)))))
-)
+      (conj (move screen (last entities))))))
         
 
 (defgame clo-runner-game
