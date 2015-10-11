7da4bf41d881f86fc106d1b9e4b4861944454d8a
Jelena Tabas <jelenatjelena@gmail.com>
2015-10-11 14:44:35 +0200

    bug fix

--- desktop/src-common/clo_runner/core.clj
+++ desktop/src-common/clo_runner/core.clj
@@ -25,7 +25,9 @@
                                                 (- (:x entity) player-speed) 
                                                 0)
                                             (assoc entity :x (- (:x entity) player-speed))
-                                            (assoc entity :x 0))))
+                                            (assoc entity :x 0))
+    :else entity
+    ))
 
 
 (defn create-box []  
@@ -89,34 +91,37 @@
       (throw (Exception. "Game over"))
       (conj (apply conj (conj [] backgr) boxes) player))))
 
+(defn first-screen [screen entities]
+  (update! screen :renderer (stage))
+  (add-timer! screen :spawn-enemy 0 2)
+  (let [
+        sheet (texture "bird.png")
+        birdy (texture! sheet :split (/ 506 3) (/ 918 5))
+        
+        background (texture "background.jpg")
+        background (assoc background :x 0 :y 0 :width 800 :height 600)                
+          
+        player-image (texture (aget birdy 0 0))
+        ;;images (->
+        ;;       []
+        ;;     (conj (texture (aget birdy 0 0)))
+        ;;   (conj (texture (aget birdy 0 1)))
+                ;;   (conj (texture (aget birdy 0 2)))                   
+        ;;   (conj (texture (aget birdy 0 3)))
+        ;;   (conj (texture (aget birdy 0 4)))
+        ;;   (conj (texture (aget birdy 1 0)))
+                ;;   (conj (texture (aget birdy 1 1)))
+        ;;   (conj (texture (aget birdy 1 2)))                  
+        ;;   (conj (texture (aget birdy 1 3)))
+        ;;   (conj (texture (aget birdy 1 4))))
+        ;;     player-image (assoc player-image :moving (animation 0.15 images))
+        player-image (assoc player-image :x 10 :y 10 :width 59 :height 59)]  
+    [background player-image]))
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
-    (update! screen :renderer (stage))
-    (add-timer! screen :spawn-enemy 0 2)
-    (let [
-          sheet (texture "bird.png")
-          birdy (texture! sheet :split (/ 506 3) (/ 918 5))
-          
-          background (texture "background.jpg")
-          background (assoc background :x 0 :y 0 :width 800 :height 600)                
-          
-          player-image (texture (aget birdy 0 0))
-          ;;images (->
-            ;;       []
-              ;;     (conj (texture (aget birdy 0 0)))
-                ;;   (conj (texture (aget birdy 0 1)))
-                ;;   (conj (texture (aget birdy 0 2)))                   
-                ;;   (conj (texture (aget birdy 0 3)))
-                ;;   (conj (texture (aget birdy 0 4)))
-                ;;   (conj (texture (aget birdy 1 0)))
-                ;;   (conj (texture (aget birdy 1 1)))
-                ;;   (conj (texture (aget birdy 1 2)))                  
-                ;;   (conj (texture (aget birdy 1 3)))
-                ;;   (conj (texture (aget birdy 1 4))))
-     ;;     player-image (assoc player-image :moving (animation 0.15 images))
-          player-image (assoc player-image :x 10 :y 10 :width 59 :height 59)]  
-      [background player-image]))  
+    (first-screen screen entities))  
   
   :on-render
   (fn [screen entities]
@@ -127,12 +132,18 @@
                  (moving entities)
                  (collision))
                (catch Exception e 
-                 [(assoc (texture "gameover.jpg")
-                        :x 0 :y 0 :width 800 :height 600)])))) 
+                 [(->
+                    (texture "gameover.jpg")
+                    (assoc :x 0 :y 0 :width 800 :height 600))           
+                  ])))) 
        
   :on-timer
   (fn [screen entities]
     :spawn-enemy (conj (conj (vec (drop-last entities)) (create-box)) (last entities)))
+  
+  :on-touch-down
+  (fn [screen entities]
+    (first-screen screen entities))
   
   :on-key-down
   (fn [screen entities]
@@ -141,6 +152,7 @@
       (vec)
       (conj (move screen (last entities))))))
         
+ 
 
 (defgame clo-runner-game
   :on-create
